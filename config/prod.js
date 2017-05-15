8dc6767c1f175957f19ebb6878f88aabb2e13865
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-05-14 20:05:32 -0500

    Mongo Url config - Docker

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   db: {
-    url: 'mongodb://127.0.0.1:27017/adopta-pets',
+    url: 'mongodb://' + process.env.MONGODB_PORT_27017_TCP_ADDR + ':' + process.env.MONGODB_PORT_27017_TCP_PORT + '/adopta-pets',
   },
   secret: 'ce1d9858-8a8c-4720-8c44-b2de67595166',
   url: 'http://localhost:3000',
