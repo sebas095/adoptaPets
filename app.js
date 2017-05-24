575c56d1848813525dbc109236ca7cd0ee4f8b2a
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-05-24 17:41:29 -0500

    change static root

--- app.js
+++ app.js
@@ -35,11 +35,13 @@ app.set("view engine", "ejs");
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/adopta-pets", express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
 
 app.use(
