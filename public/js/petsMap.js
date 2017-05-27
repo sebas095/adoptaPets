7ff3f7b9ad27b89997992845b0f68731b4873830
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-05-27 01:18:36 -0500

    add map with results in the search

--- public/js/petsMap.js
+++ public/js/petsMap.js
@@ -20,29 +20,132 @@
 
     osmGeocoder = new L.Control.OSMGeocoder({ text: "Localizar" });
     map.addControl(osmGeocoder);
+
     locate(map);
+    const pets = getData();
+    addPetstoMap(map, pets);
   }
 
-  function addMarker(location) {
+  function addMyMarker(location) {
     if (marker) {
       map.removeLayer(marker);
     }
-    marker = L.marker(location, { draggable: true });
+    marker = L.marker(location, { draggable: false });
     marker.addTo(map);
+    map.panTo(location);
+    marker.bindPopup("!Estoy aquí").openPopup();
+  }
+
+  function addPetstoMap(map, pets) {
+    const icon = L.icon({
+      iconUrl: "/adopta-pets/img/markers/position.png",
+      shadowUrl: "/adopta-pets/img/markers/shadow.png",
+      iconSize: [48, 48], // size of the icon
+      shadowSize: [48, 48], // size of the shadow
+      iconAnchor: [24, 48], // point of the icon which will correspond to marker's location
+      shadowAnchor: [7, 40], // the same for the shadow
+      popupAnchor: [0, -48] // point from which the popup should open relative to the iconAnchor
+    });
+
+    for (let i = 0, pet = pets[i]; i < pets.length; i++) {
+      const petMarker = L.marker(new L.LatLng(pet.lat, pet.lng), {
+        draggable: false,
+        icon
+      });
+      petMarker.addTo(map);
+      petMarker.bindPopup(
+        `<div class="row">
+           <div class="col s12 m12">
+             <div class="card">
+               <div class="card-image">
+                 <img src="${pet.photo}" width="100" height="100">
+                 <span class="card-title orange-text" style="text-align: center !important">${pet.name}</span>
+               </div>
+               <div class="card-content">
+                 <div class="row">
+                   <h5 class="center amber-text icon ap-cat-7"> Características</h5>
+                   <div class="input-field col l6 m12 s6">
+                     <i class="material-icons prefix">color_lens</i>
+                     <input type="text" id="pet.color" name="pet.color" readonly="" value="${pet.color}" aria-required="true">
+                     <label for="pet.color">Color</label>
+                   </div>
+                   <div class="input-field col l6 m12 s6">
+                     <i class="material-icons prefix">zoom_out_map</i>
+                     <input type="text" id="pet.size" name="pet.size" readonly="" value="${pet.size}" aria-required="true">
+                     <label for="pet.size">Tamaño</label>
+                   </div>
+                 </div>
+                 <div class="row">
+                   <div class="input-field col l6 m6 s12">
+                     <div class="col s8">
+                       <i class="material-icons prefix">add_circle</i>
+                       <input type="number" id="pet.age" name="pet.age" readonly="" value="${pet.age}">
+                       <label for="pet.age">Edad</label>
+                     </div>
+                     <div class="col s4">
+                       <input name="pet.time" id="pet.time" readonly value="${pet.time}" aria-required="true">
+                     </div>
+                   </div>
+                   <div class="input-field col l6 m12 s6">
+                     <i class="material-icons prefix">pets</i>
+                     <input type="text" id="pet.gender" name="pet.gender" readonly="" value="pet.gender" aria-required="true">
+                     <label for="pet.gender">Sexo</label>
+                   </div>
+                 </div>
+                 <div class="row">
+                   <div class="input-field col m6 s12">
+                     <i class="icon ap-dog-2 prefix"></i>
+                     <input type="text" id="pet.type" name="pet.type" readonly="" value="${pet.type}" aria-required="true">
+                     <label for="pet.type">Tipo</label>
+                   </div>
+                 </div>
+                 <div class="row">
+                   <div class="input-field col l6 offset-m3 m12 s6">
+                     <a class="btn waves-effect waves-light cyan darken-2 white-text" href="/adopta-pets/publications/${pet.id}">Ver más</a>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>`
+      );
+    }
   }
 
   function locate(map) {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
+      navigator.geolocation.watchPosition(
         position => {
           const { latitude, longitude } = position.coords;
           const initLocation = new L.LatLng(latitude, longitude);
           map.panTo(initLocation);
-          addMarker(new L.LatLng(latitude, longitude));
+          addMyMarker(new L.LatLng(latitude, longitude));
         },
         err => console.log(err),
         { timeout: 10000 }
       );
     }
+  }
+
+  function getData() {
+    let pets = [];
+    const data = document.querySelectorAll(".data");
+    for (let i = 0; i < data.length; i += 11) {
+      const pet = {
+        photo: data[i].value,
+        id: data[i + 1].value,
+        name: data[i + 2].value,
+        color: data[i + 3].value,
+        size: data[i + 4].value,
+        age: data[i + 5].value,
+        time: data[i + 6].value,
+        gender: data[i + 7].value,
+        type: data[i + 8].value,
+        lat: parseFloat(data[i + 9].value),
+        lng: parseFloat(data[i + 10].value)
+      };
+      pets.push(pet);
+    }
+    return pets;
   }
 })();
